056dbc8cef3a283da100633a6c6ff164eeaadbb8
Sarkhan <abdullayevsarxan92@gmail.com>
2024-02-22 02:14:28 +0400

    adding registration via google logic

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,24 @@
+"use client"
 import Link from "next/link";
 import Social from "@/components/Social";
+import { signInWithGoogle } from "@/lib/firebase/firebaseConfig";
+
+
 
 
 const Header: React.FC = () => {
+
+  const handleLogin = async () => {
+    try {
+      const user = await signInWithGoogle();
+      if (user) {
+        console.log(user);
+      }
+    } catch (error) {
+      console.error('Ошибка входа:', error);
+    }
+  };
+
   return (
     <header className="fixed top-0 z-[40] w-full h-[100px] flex bg-transparent justify-between items-center px-10 md:px-20 p-4">
       <nav className="flex space-x-4 mt-2 gap-10">
@@ -11,7 +27,7 @@ const Header: React.FC = () => {
       </nav>
 
       <div className="ml-auto">
-        <Link href="/login" className="text-gray-400 hover:text-neutral-600 font-semibold border-solid border-2 border-gray-400 rounded-[20px] px-5 py-2">Log in</Link>
+        <button onClick={handleLogin} className="text-gray-400 hover:text-neutral-600 font-semibold border-solid border-2 border-gray-400 rounded-[20px] px-5 py-2">Log in</button>
         <Social />
       </div>
 
--- src/lib/firebase/firebaseConfig.ts
+++ src/lib/firebase/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApp, getApps, FirebaseApp } from 'firebase/app';
 import { Firestore, getFirestore } from 'firebase/firestore';
-
+import { getAuth, GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCPP1vFH-j44dTveCE_YaClsPooUk38G90",
@@ -16,5 +16,20 @@ const firebaseApp: FirebaseApp = !getApps().length ? initializeApp(firebaseConfi
 console.log("Firebase App Initialized:", firebaseApp);
 const db: Firestore = getFirestore(firebaseApp);
 console.log("Firestore DB Reference:", db);
+const auth = getAuth(firebaseApp);
 
 export const getDb = () => db;
+
+
+export const signInWithGoogle = async (): Promise<User | null> => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    console.log('Успешный вход:', user);
+    return user;
+  } catch (error) {
+    console.error('Ошибка аутентификации:', error);
+    return null;
+  }
+};
