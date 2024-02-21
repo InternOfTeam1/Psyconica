c11e0452ce6e83c25a638fc358976a9baed78d68
Veronika4Donchenko4 <donchenko.veronika96@gmail.com>
2024-02-21 17:08:18 +0100

    added links for topic (pages are not created, I'll add them after the call)

--- src/components/SubjectsList.tsx
+++ src/components/SubjectsList.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { fetchDataFromCollection } from '@/lib/firebase/firebaseUtils'; 
+import Link from 'next/link';
+
 
 interface Subject {
   id: string;
@@ -33,16 +35,17 @@ const SubjectsList: React.FC<{ position: 'left' | 'right' }> = ({ position }) =>
   return (
   <div className="grid grid-cols-1 gap-4 sm:p-6 md:gap-6 lg:gap-4 ">
 {subjectsToShow.map((subject) => (
+  <Link key={subject.id} href={`/subject/${subject.id}`}>
   <div 
-    key={subject.id} 
-    className="rounded-lg space-y-4 p-4  sm:p-6 md:space-y-3 lg:space-y-4 md:p-4 lg:p-8 flex flex-col justify-center items-start bg-cover bg-center"
-     style={{ backgroundImage: `url(${backgroundImageUrl})`, minWidth: '400px', minHeight: '150px' }}
+    className="rounded-lg space-y-4 p-4 sm:p-6 md:space-y-3 lg:space-y-4 md:p-4 lg:p-8 flex flex-col justify-center items-start bg-cover bg-center"
+    style={{ backgroundImage: `url(${backgroundImageUrl})`, minWidth: '400px', minHeight: '150px' }}
   >
-    <div className=" sm:pl-10 sm:pr-10 md:pl-5 md:pr-10 lg:pl-8 lg:pr-5  font-balsamiq-sans font-bold text-small-caps"> 
-      <h2 className=" pl-6 pr-1 sm:text-xl md:text-2xl  lg:text-2xl text-gray-600">{subject.title}</h2>
+    <div className="sm:pl-10 sm:pr-10 md:pl-5 md:pr-10 lg:pl-8 lg:pr-5 font-balsamiq-sans font-bold text-small-caps"> 
+      <h2 className="pl-6 pr-1 sm:text-xl md:text-2xl lg:text-2xl text-gray-600">{subject.title}</h2>
       <p className="text-sm sm:text-base md:text-base lg:text-lg">{subject.description}</p>
     </div>
   </div>
+</Link>
 ))}
 </div>
 );
