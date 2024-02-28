c81e52e23a038a26eccfba91cd11dee8af314c8a
Sarkhan <abdullayevsarxan92@gmail.com>
2024-02-29 00:01:12 +0400

    adding back button and wrap layout with Provider from redux

--- src/redux/Provider.tsx
+++ src/redux/Provider.tsx
@@ -1,10 +1,10 @@
-// 'use client'
-// import { Provider } from "react-redux";
-// import { store } from "@/redux/store";
+'use client'
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 
-// function Providers({ children }: { children: React.ReactNode }) {
-//   return <Provider store={store}>{children}</Provider>;
-// }
+function Providers({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
 
-// export default Providers;
+export default Providers;
