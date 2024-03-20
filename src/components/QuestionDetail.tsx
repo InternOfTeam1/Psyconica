e98a292d979de01436fbf9668fb6e25c4ce7e3f6
AleksandrKompanetc <moneymacker7@gmail.com>
2024-03-21 00:33:21 +0200

    changed block of question

--- src/components/QuestionDetail.tsx
+++ src/components/QuestionDetail.tsx
@@ -47,18 +47,18 @@ const QuestionDetail = () => {
   // };
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 py-4 max-w-xl bg-white shadow-xl rounded-2xl">
 
 
       {questionData && questionData.answers.map((answer: Answers, index: number) => (
         <>
-          <h2 className="text-xl font-semibold">{questionData.title}</h2>
-          <div key={index} className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold bg-amber-300 text-gray-600 px-7 py-3 rounded-2xl leading-6">{questionData.title}</h2>
+          <div key={index} className="bg-white p-6 rounded-lg">
             <h2>{answer.title}</h2>
           </div>
         </>
 
-      ))}
+      ))} 
 
       <Link href={HOME_ROUTE}>
         <button className="inline-block mt-4 px-6 py-2 text-sm font-medium leading-6 text-center text-white uppercase transition bg-blue-500 rounded-full shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-blue-600">
