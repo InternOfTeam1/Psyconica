ee702af85aef6560ac87c706ec0b518bc46059b9
Sarkhan <abdullayevsarxan92@gmail.com>
2024-03-24 15:26:43 +0400

    refactoring: change logic for like, and add like logic also for visitors

--- src/components/QuestionDetail.tsx
+++ src/components/QuestionDetail.tsx
@@ -8,34 +8,47 @@ import { HOME_ROUTE } from '@/constants/routes';
 import { useParams } from 'next/navigation';
 import { useAppSelector } from '../redux/hooks';
 import { updateAnswerLikes } from '@/lib/firebase/firebaseFunctions';
+import Cookies from 'js-cookie';
 
 
 function fetchQuestionData(slug: {}) {
   return fetchDoc('questions', slug);
 }
 
-const MAX_LIKES = 100;
+function generateGuestId() {
+  let guestId = Cookies.get('guestId');
+  if (!guestId) {
+    guestId = crypto.randomUUID();
+    Cookies.set('guestId', guestId, { expires: 3650 }); // Установка cookies на 10 лет
+  }
+  return guestId;
+}
+
+
 
 const QuestionDetail = () => {
   const [questionData, setQuestionData] = useState<{ title: string; answers: Answers[] } | null>(null);
-  // const [likedAnswers, setLikedAnswers] = useState<string[]>([]);
   const params = useParams();
   const questionSlug: any = params.slug;
   const userId = useAppSelector((state) => state.auth.user?.id);
+  const MAX_LIKES = 100;
+  let userOrGuestId: any = userId || generateGuestId();
 
   const handleLikeClick = async (answerNum: any, answerLikes: string[]) => {
-    if (!userId) {
+    const isLiked = answerLikes.includes(userOrGuestId);
+    let updatedLikes;
+
+    if (!userOrGuestId) {
       console.error("User ID is undefined.");
       return;
     }
-    let updatedLikes;
-    const isLiked = answerLikes.includes(userId);
+
     if (isLiked) {
-      updatedLikes = answerLikes.filter(id => id !== userId);
+      updatedLikes = answerLikes.filter(id => id !== userOrGuestId);
       console.log(updatedLikes)
 
     } else {
-      updatedLikes = [...answerLikes, userId];
+      updatedLikes = [...answerLikes, userOrGuestId];
     }
     try {
       await updateAnswerLikes(answerNum, updatedLikes, questionSlug);
@@ -69,7 +82,7 @@ const QuestionDetail = () => {
         <>
           <h2 className="font-semibold bg-amber-300 text-gray-600 px-7 py-3 rounded-2xl leading-6 text-center xs:text-sm xs:px-3 sm:text-sm sm:px-4 md:text-base md:px-5 lg:text-lg lg:px-6 xl:text-lg xl:px-7">{questionData.title}</h2>
           {questionData.answers.map((answer: Answers, index: number) => {
-             const progressWidth = (answer.likes.length / MAX_LIKES) * 100;
+            const progressWidth = (answer.likes.length / MAX_LIKES) * 100;
             return (
               <div key={index} className="mt-4">
                 <div className="flex items-start mb-4">
@@ -80,21 +93,21 @@ const QuestionDetail = () => {
                   </div>
                 </div>
                 <div className="flex items-center">
-                <FaThumbsUp
-            className={`text-green-500 mr-1 cursor-pointer ${answer.likes.includes(userId) ? 'text-green-700' : ''}`}
-            onClick={() => handleLikeClick(answer.num, answer.likes)} />
-          <span className="text-gray-500">{answer.likes.length}</span>
-          <div style={{ flex: 1, marginLeft: '10px', marginRight: '10px' }}>
-            <div style={{ height: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
-              <div style={{ width: `${progressWidth}%`, height: '100%', backgroundColor: '#00b894', borderRadius: 'inherit' }}></div>
-            </div>
-          </div>
-        </div>
-        <hr className="my-4 border-gray-400" />
-      </div>
-    );
-  })}
-</>
+                  <FaThumbsUp
+                    className={`text-green-500 mr-1 cursor-pointer ${answer.likes.includes(userOrGuestId) ? 'text-green-700' : ''}`}
+                    onClick={() => handleLikeClick(answer.num, answer.likes)} />
+                  <span className="text-gray-500">{answer.likes.length}</span>
+                  <div className="flex-1 mx-2.5">
+                    <div className="h-2.5 border border-gray-300 rounded">
+                      <div className="h-full bg-teal-400 rounded" style={{ width: `${progressWidth}%` }}></div>
+                    </div>
+                  </div>
+                </div>
+                <hr className="my-4 border-gray-400" />
+              </div>
+            );
+          })}
+        </>
       )}
       <Link href={HOME_ROUTE}>
         <button className="inline-block mt-4 px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-500 rounded-full shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-blue-600 xs:text-xs sm:text-xs md:text-xs lg:text-sm xl:text-sm">
