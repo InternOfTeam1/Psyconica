cb7912587ad7bfdf338de687b5d54c28158f7a8e
Veronika4Donchenko4 <donchenko.veronika96@gmail.com>
2024-03-24 16:44:20 +0700

    added progress bar for likes

--- src/components/QuestionDetail.tsx
+++ src/components/QuestionDetail.tsx
@@ -14,6 +14,8 @@ function fetchQuestionData(slug: {}) {
   return fetchDoc('questions', slug);
 }
 
+const MAX_LIKES = 100;
+
 const QuestionDetail = () => {
   const [questionData, setQuestionData] = useState<{ title: string; answers: Answers[] } | null>(null);
   // const [likedAnswers, setLikedAnswers] = useState<string[]>([]);
@@ -67,6 +69,7 @@ const QuestionDetail = () => {
         <>
           <h2 className="font-semibold bg-amber-300 text-gray-600 px-7 py-3 rounded-2xl leading-6 text-center xs:text-sm xs:px-3 sm:text-sm sm:px-4 md:text-base md:px-5 lg:text-lg lg:px-6 xl:text-lg xl:px-7">{questionData.title}</h2>
           {questionData.answers.map((answer: Answers, index: number) => {
+             const progressWidth = (answer.likes.length / MAX_LIKES) * 100;
             return (
               <div key={index} className="mt-4">
                 <div className="flex items-start mb-4">
@@ -77,16 +80,21 @@ const QuestionDetail = () => {
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <FaThumbsUp
-                    className={`text-green-500 mr-1 cursor-pointer ${answer.likes.includes(userId) ? 'text-green-700' : ''}`}
-                    onClick={() => handleLikeClick(answer.num, answer.likes)} />
-                  <span className="text-gray-500">{answer.likes.length}</span>
-                </div>
-                <hr className="my-4 border-gray-400" />
-              </div>
-            );
-          })}
-        </>
+                <FaThumbsUp
+            className={`text-green-500 mr-1 cursor-pointer ${answer.likes.includes(userId) ? 'text-green-700' : ''}`}
+            onClick={() => handleLikeClick(answer.num, answer.likes)} />
+          <span className="text-gray-500">{answer.likes.length}</span>
+          <div style={{ flex: 1, marginLeft: '10px', marginRight: '10px' }}>
+            <div style={{ height: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
+              <div style={{ width: `${progressWidth}%`, height: '100%', backgroundColor: '#00b894', borderRadius: 'inherit' }}></div>
+            </div>
+          </div>
+        </div>
+        <hr className="my-4 border-gray-400" />
+      </div>
+    );
+  })}
+</>
       )}
       <Link href={HOME_ROUTE}>
         <button className="inline-block mt-4 px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-500 rounded-full shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-blue-600 xs:text-xs sm:text-xs md:text-xs lg:text-sm xl:text-sm">
