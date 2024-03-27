c99fd8058dc2fbdb5da2263a8422ae114796f562
AleksandrKompanetc <moneymacker7@gmail.com>
2024-03-28 00:26:45 +0200

    created CRUD for answers

--- src/components/QuestionDetail.tsx
+++ src/components/QuestionDetail.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FaThumbsUp } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md';
 import { fetchDoc } from '@/lib/firebase/firebaseGetDocs';
 import { useEffect, useState } from 'react';
 import { Answers } from '@/interfaces/collections';
@@ -7,8 +8,9 @@ import Link from 'next/link';
 import { HOME_ROUTE } from '@/constants/routes';
 import { useParams } from 'next/navigation';
 import { useAppSelector } from '../redux/hooks';
-import { updateAnswerLikes } from '@/lib/firebase/firebaseFunctions';
+import { updateAnswerLikes, updateQuestion } from '@/lib/firebase/firebaseFunctions';
 import Cookies from 'js-cookie';
+import { nanoid } from '@reduxjs/toolkit';
 
 
 function fetchQuestionData(slug: {}) {
@@ -24,14 +26,14 @@ function generateGuestId() {
   return guestId;
 }
 
-
-
 const QuestionDetail = () => {
   const [questionData, setQuestionData] = useState<{ title: string; answers: Answers[] } | null>(null);
   const params = useParams();
   const questionSlug: any = params.slug;
   const userId = useAppSelector((state) => state.auth.user?.id);
+  const userRole = useAppSelector((state) => state.auth.user?.role);
   const MAX_LIKES = 100;
+
   let userOrGuestId: any = userId || generateGuestId();
 
   const handleLikeClick = async (answerNum: any, answerLikes: string[]) => {
@@ -76,6 +78,53 @@ const QuestionDetail = () => {
     fetchData();
   }, [questionSlug]);
 
+
+  const onAnswerChange = (e, answerNumber, field = 'title') => {
+    const newValue = e.target.value;
+
+    console.log(questionData)
+    const answers = questionData?.answers;
+
+    const changedAnswers = answers?.map(answer => answerNumber === answer.num ? { ...answer, [field]: newValue } : answer);
+
+    setQuestionData({
+      ...questionData,
+      answers: changedAnswers ?? []
+    })
+  }
+
+  
+  const onAnswerAdd = () => {
+    const answers = questionData?.answers;
+
+    setQuestionData({
+      ...questionData,
+      answers: [...answers, {
+        content: '',
+        likes: [],
+        num: nanoid(),
+        title: ''
+      }]
+    })
+  }
+
+
+  const onSave = async () => {
+    await updateQuestion(questionSlug, questionData)
+  }
+
+  const onAnswerDelete = (answerNum) => {
+    const answers = questionData?.answers;
+
+    const newAnswers = answers?.filter(answer => answer.num !== answerNum)
+
+    setQuestionData({
+      ...questionData,
+      answers: newAnswers
+    })
+  }
+
+
   return (
     <div className="container mx-auto px-4 py-4 max-w-xl bg-white shadow-xl rounded-2xl">
       {questionData && (
@@ -84,14 +133,49 @@ const QuestionDetail = () => {
           {questionData.answers.map((answer: Answers, index: number) => {
             const progressWidth = (answer.likes.length / MAX_LIKES) * 100;
             return (
-              <div key={index} className="mt-4">
+              <div key={index} className="mt-4 w-full">
                 <div className="flex items-start mb-4">
-                  <p className="font-semibold text-gray-600 mr-2">{answer.num}.</p>
-                  <div>
-                    <h3 className="font-semibold text-gray-600 leading-6">{answer.title}</h3>
-                    <p className="text-gray-500 text-sm mt-2">{answer.content}</p>
-                  </div>
+                  <p className="font-semibold text-gray-600 mr-2">{index + 1}.</p>
+                  {userRole === 'psy' ? (
+                    <>
+                      <div className='w-full'>
+                        <h3 className="font-semibold text-gray-600 leading-6">
+                          <input
+                            type="text"
+                            className='w-full'
+                            value={answer.title}
+                            onChange={(e) => onAnswerChange(e, answer.num)}
+                            placeholder="Enter your answer"
+                          />
+                        </h3>
+                        <p className="text-gray-500 text-sm mt-2 w-full">
+                          <textarea
+                            value={answer.content}
+                            className='w-full h-12'
+                            onChange={(e) => onAnswerChange(e, answer.num, 'content')}
+                            placeholder="Enter your description"
+                          />
+                        </p>
+                      </div>
+                      <div className='cursor-pointer' onClick={() => onAnswerDelete(answer.num)}>
+                        <MdClose />
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className='w-full'>
+                        <h3 className="font-semibold text-gray-600 leading-6">
+                          {answer.title}
+                        </h3>
+                        <p className="text-gray-500 text-sm mt-2 w-full">
+                          {answer.content}
+                        </p>
+                      </div>
+                    </>
+                  )}
                 </div>
+
+
                 <div className="flex items-center">
                   <FaThumbsUp
                     className={`text-green-500 mr-1 cursor-pointer ${answer.likes.includes(userOrGuestId) ? 'text-green-700' : ''}`}
@@ -108,13 +192,28 @@ const QuestionDetail = () => {
             );
           })}
         </>
-      )}
+      )
+      }
+
+
+    {userRole === 'psy' ? (
+      <>
+        <button className="inline-block mt-4 px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-500 rounded-full shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-blue-600 xs:text-xs sm:text-xs md:text-xs lg:text-sm xl:text-sm"
+                onClick={onAnswerAdd}>Add Answer</button>
+        <button className="inline-block mt-4 px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-500 rounded-full shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-blue-600 xs:text-xs sm:text-xs md:text-xs lg:text-sm xl:text-sm"
+                onClick={onSave}>Save </button>
+      </>
+      ) : null}
+
+      <br />
+
       <Link href={HOME_ROUTE}>
         <button className="inline-block mt-4 px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-500 rounded-full shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-blue-600 xs:text-xs sm:text-xs md:text-xs lg:text-sm xl:text-sm">
           Вернуться на главную
         </button>
       </Link>
-    </div>
+
+    </div >
   );
 };
 
