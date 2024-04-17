781f0c578c7b0f7f2ed81d5140f9f80f7c75bba0
Sarkhan <abdullayevsarxan92@gmail.com>
2024-04-17 22:59:16 +0400

    change h1 for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,10 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center mx-auto" >
-      <h2 className="text-center  sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-balsamiq-sans font-bold text-pink-600 mb-2">
-        Психология должна быть простой!
-      </h2>
+      <h1 className="sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 leading-none text-center shadow-lg">
+        Психологическая ясность для современной жизни
+      </h1>
+
 
       <div className="z-30 mx-auto flex flex-col items-center min-w-full ">
         <div className="z-30 flex flex-col lg:flex-row items-center justify-center ">
