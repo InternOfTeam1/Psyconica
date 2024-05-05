9ca2d3a62ea2c17c1656cae486a4e32a41d1e670
Sarkhan <abdullayevsarxan92@gmail.com>
2024-05-06 02:10:22 +0400

    connecting answered questions with question page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ import PsychologistModal from './PsychologistCheckbox';
 import { openModal, closeModal } from '@/redux/slices/modalSlice';
 import { PiXBold, PiListBold } from "react-icons/pi";
 import { getVideosById } from '@/lib/firebase/firebaseFunctions';
-
+import { useAppSelector } from '../redux/hooks';
 
 const Header: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -27,10 +27,11 @@ const Header: React.FC = () => {
   const [isModalOpenPsy, setIsModalOpenPsy] = useState(false);
   const router = useRouter();
   const [menuOpen, setMenuOpen] = useState(false);
-  
-  const handleNav = () =>{
+  const userId = useAppSelector((state) => state.auth.user?.id);
+
+  const handleNav = () => {
     setMenuOpen(!menuOpen)
-  } 
+  }
 
   useEffect(() => {
     function handleScroll() {
@@ -54,24 +55,41 @@ const Header: React.FC = () => {
     dispatch(closeModal());
   };
 
-//  useEffect(() => {
-//     if (isAuthenticated && user) {
-//       const userData = { name: user.name, photo: user.photo, email: user.email };
-//       Cookies.set('user', JSON.stringify(userData), { expires: 7 });
-//     }
-//   }, [isAuthenticated, user, router]);
+  //  useEffect(() => {
+  //     if (isAuthenticated && user) {
+  //       const userData = { name: user.name, photo: user.photo, email: user.email };
+  //       Cookies.set('user', JSON.stringify(userData), { expires: 7 });
+  //     }
+  //   }, [isAuthenticated, user, router]);
 
   useEffect(() => {
     const userCookie = Cookies.get('user');
-    if(userCookie){
+    if (userCookie) {
       dispatch(setUserState(JSON.parse(userCookie as string)));
       console.log(JSON.parse(userCookie as string))
     }
-  },[dispatch])
+  }, [dispatch])
 
   useEffect(() => {
     getVideosById(user.id);
-  },[])
+  }, [])
+
+  const handleClick = async (user: string) => {
+    if (!user) {
+      handleOpenModal()
+
+      console.error("User ID is undefined.");
+      return;
+    }
+
+    try {
+
+      await router.push(`/profile/${user}`);
+
+    } catch (error) {
+      console.error('Ошибка навигации:', error);
+    }
+  };
 
   const handleLogin = async (provider: 'google' | 'facebook') => {
     try {
@@ -111,27 +129,27 @@ const Header: React.FC = () => {
           <div onClick={handleNav} className='sm:hidden cursor-pointer pl-5 pt-5 absolute top-0 left-0 text-gray-500  hover:text-white'>
             <PiListBold size={25} />
           </div>
-      )}
-     <nav className="gap-2 hidden sm:flex">
-      <Link href="/questions" className=" cursor-pointer text-gray-500  hover:bg-neutral-500  hover:rounded-full hover:text-white font-semibold xs:text-xs sm:text-sm md:text-sm lg:order-3 lg:text-base xl:order-3 mt-5 py-1 px-5 hidden sm:flex">Вопросы</Link>
-      <Link href="/articles" className="cursor-pointer text-gray-500  hover:bg-neutral-500  hover:rounded-full hover:text-white font-semibold xs:text-xs sm:text-sm md:text-sm lg:order-3 lg:text-base xl:order-3 mt-5 py-1 px-5 hidden sm:flex">Статьи</Link>
-    </nav>
-    <div className="flex items-center justify-center">
-    <Link href={HOME_ROUTE} className="flex items-center mt-5 lg:order-3 md:order-3 text-center xs:max-w-[80%] lg:max-w-[100%]">
-      <Image
-        src="/siteName.webp"
-        alt="website name"
-        width={400}
-        height={100}
-        className="max-w-full h-full object-cover"
-        priority={true}
-      />
-    </Link>
-    </div>
-    <div className="xs:order-2 sm:order-2 sm:mr-10 md:order-2 md:mr-30 lg:order-3 xl:order-3 xl:mr-20 mr-5 ">
-      <div className='xs:ml-4'>
-        <Social />
+        )}
+        <nav className="gap-2 hidden sm:flex">
+          <Link href="/questions" className=" cursor-pointer text-gray-500  hover:bg-neutral-500  hover:rounded-full hover:text-white font-semibold xs:text-xs sm:text-sm md:text-sm lg:order-3 lg:text-base xl:order-3 mt-5 py-1 px-5 hidden sm:flex">Вопросы</Link>
+          <Link href="/articles" className="cursor-pointer text-gray-500  hover:bg-neutral-500  hover:rounded-full hover:text-white font-semibold xs:text-xs sm:text-sm md:text-sm lg:order-3 lg:text-base xl:order-3 mt-5 py-1 px-5 hidden sm:flex">Статьи</Link>
+        </nav>
+        <div className="flex items-center justify-center">
+          <Link href={HOME_ROUTE} className="flex items-center mt-5 lg:order-3 md:order-3 text-center xs:max-w-[80%] lg:max-w-[100%]">
+            <Image
+              src="/siteName.webp"
+              alt="website name"
+              width={400}
+              height={100}
+              className="max-w-full h-full object-cover"
+              priority={true}
+            />
+          </Link>
         </div>
+        <div className="xs:order-2 sm:order-2 sm:mr-10 md:order-2 md:mr-30 lg:order-3 xl:order-3 xl:mr-20 mr-5 ">
+          <div className='xs:ml-4'>
+            <Social />
+          </div>
           <ul className="items-center gap-2 mb-4 align-middle list hidden sm:flex">
             {!isAuthenticated ? (
               <li onClick={handleOpenModal} className="flex cursor-pointer text-gray-400 hover:text-neutral-600 font-semibold xs:text-xs sm:text-sm md:text-sm lg:order-3 lg:text-base xl:order-3 border-solid border-2 border-gray-400 whitespace-nowrap rounded-[20px] mt-5 mb-5 py-1 px-5">Log in with social network</li>
@@ -139,11 +157,11 @@ const Header: React.FC = () => {
               <>
                 <Image src={userPhoto} alt="User Profile" width={30} height={30} className="w-8 h-8 rounded-full border-2 border-gray-400 shadow-sm xs:w-6 xs:h-6 sm:w-7 sm:h-7 lg:w-8 lg:h-8 mt-5 mb-5 " />
 
-                <Link href={PROFILE_ROUTE}>
-                <li className=" list-item cursor-pointer text-gray-500 hover:text-neutral-600 font-semibold xs:text-xs sm:text-sm md:text-sm lg:text-base border-solid border-2 border-gray-400 whitespace-nowrap rounded-[20px] mt-5 mb-5 px-5 py-1 hover:bg-gray-200">
-                    Личный кабинет
-                  </li>
-                </Link>
+                {/* <Link href={PROFILE_ROUTE}> */}
+                <li className=" list-item cursor-pointer text-gray-500 hover:text-neutral-600 font-semibold xs:text-xs sm:text-sm md:text-sm lg:text-base border-solid border-2 border-gray-400 whitespace-nowrap rounded-[20px] mt-5 mb-5 px-5 py-1 hover:bg-gray-200 " onClick={() => handleClick(userId)} >
+                  Личный кабинет
+                </li>
+                {/* </Link> */}
                 <li onClick={handleLogout} className=" list-item cursor-pointer text-gray-500 hover:text-neutral-600 font-semibold xs:text-xs sm:text-sm md:text-sm lg:text-base border-solid border-2 border-gray-400 whitespace-nowrap rounded-[20px] mt-5 mb-5 px-5 py-1 hover:bg-gray-200">
                   Log out
                 </li>
@@ -152,94 +170,97 @@ const Header: React.FC = () => {
           </ul>
         </div>
         <div className={
-          menuOpen 
-          ? "fixed top-0 left-0 w-[75%] sm:hidden p-7 ease-in duration-500 max-h-screen overflow-hidden bg-gradient-to-r from-purple-300 to-transparent z-50"
-          : "fixed left-[-100%] top-0 p-10 ease-in duration-500 bg-gradient-to-r from-purple-300 to-transparent z-0"
-        }> 
-        <div className='flex w-full items-center justify-start'>
-          <div onClick={handleNav} className='cursor-pointer text-black hover:text-white'>
-            <PiXBold size={25} />
+          menuOpen
+            ? "fixed top-0 left-0 w-[75%] sm:hidden p-7 ease-in duration-500 max-h-screen overflow-hidden bg-gradient-to-r from-purple-300 to-transparent z-50"
+            : "fixed left-[-100%] top-0 p-10 ease-in duration-500 bg-gradient-to-r from-purple-300 to-transparent z-0"
+        }>
+          <div className='flex w-full items-center justify-start'>
+            <div onClick={handleNav} className='cursor-pointer text-black hover:text-white'>
+              <PiXBold size={25} />
+            </div>
+          </div>
+          <div className='flex-col py-1.5'>
+            <ul>
+              <Link href={HOME_ROUTE} className='py-1.5 cursor-pointer text-black font-bold  hover:text-white'>
+                <li onClick={() => setMenuOpen(false)}
+                  className='py-1.5 cursor-pointer text-black font-bold  hover:text-white'>
+                  Главная
+                </li>
+              </Link>
+              <Link href="/questions">
+                <li onClick={() => setMenuOpen(false)}
+                  className='py-1.5 cursor-pointer text-black font-bold  hover:text-white'>
+                  Вопросы
+                </li>
+              </Link>
+              <Link href="/articles">
+                <li onClick={() => setMenuOpen(false)}
+                  className='py-1.5 cursor-pointer text-black font-bold hover:text-white'>
+                  Статьи
+                </li>
+              </Link>
+              {!isAuthenticated ? (
+                <li onClick={() => { handleOpenModal(); setMenuOpen(false); }} className="py-1.5 cursor-pointer text-black font-bold  hover:text-white">Log in with social network</li>
+              ) : (
+                <>
+                  <Link href={PROFILE_ROUTE}>
+                    <li onClick={() => setMenuOpen(false)} className="py-1.5 cursor-pointer text-black font-bold  hover:text-white">
+                      Личный кабинет
+                    </li>
+                  </Link>
+                  <li onClick={() => { handleLogout(); setMenuOpen(false); }} className="py-1.5 cursor-pointer text-black font-bold  hover:text-white">
+                    Log out
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
         </div>
-        <div className='flex-col py-1.5'>
-          <ul>
-          <Link href={HOME_ROUTE} className='py-1.5 cursor-pointer text-black font-bold  hover:text-white'>
-           <li onClick={() => setMenuOpen(false)}
-            className='py-1.5 cursor-pointer text-black font-bold  hover:text-white'>
-              Главная
-            </li>
-           </Link>
-          <Link href="/questions">
-            <li onClick={() => setMenuOpen(false)}
-            className='py-1.5 cursor-pointer text-black font-bold  hover:text-white'>
-              Вопросы
-            </li>
-          </Link>
-          <Link href="/articles">
-            <li onClick={() => setMenuOpen(false)}
-            className='py-1.5 cursor-pointer text-black font-bold hover:text-white'>
-              Статьи
-            </li>
-          </Link>
-            {!isAuthenticated ? (
-              <li onClick={() => { handleOpenModal(); setMenuOpen(false); }} className="py-1.5 cursor-pointer text-black font-bold  hover:text-white">Log in with social network</li>
-            ) : (
-              <>
-                <Link href={PROFILE_ROUTE}>
-                <li onClick={() => setMenuOpen(false)} className="py-1.5 cursor-pointer text-black font-bold  hover:text-white">
-                    Личный кабинет
-                  </li>
-                </Link>
-                <li onClick={() => { handleLogout(); setMenuOpen(false); }} className="py-1.5 cursor-pointer text-black font-bold  hover:text-white">
-                  Log out
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-        </div>
-      </header>
+      </header >
       {isModalOpenPsy && (
         <PsychologistModal
           isOpen={isModalOpenPsy}
           onClose={() => setIsModalOpenPsy(false)}
         />
-      )}
+      )
+      }
 
-      {isModalOpen && (
-        <div className="fixed inset-0 z-50 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-screen">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" onClick={handleCloseModal}></div>
+      {
+        isModalOpen && (
+          <div className="fixed inset-0 z-50 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-screen">
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" onClick={handleCloseModal}></div>
 
-            <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-              <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">Login</h3>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">Choose your login method:</p>
-                    </div>
-                    <div className="mt-4">
-                      <button
-                        onClick={() => handleLogin('google')}
-                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
-                      >
-                        Log in with Google
-                      </button>
-                      <button
-                        onClick={() => handleLogin('facebook')}
-                        className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
-                      >
-                        Log in with Twitter
-                      </button>
+              <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+                <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                      <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">Login</h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">Choose your login method:</p>
+                      </div>
+                      <div className="mt-4">
+                        <button
+                          onClick={() => handleLogin('google')}
+                          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
+                        >
+                          Log in with Google
+                        </button>
+                        <button
+                          onClick={() => handleLogin('facebook')}
+                          className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
+                        >
+                          Log in with Twitter
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )
+      }
     </>
   );
 };
