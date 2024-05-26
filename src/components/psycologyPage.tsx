1d869a8ad39560d5a67da95de8fa06345b83a88b
Sarkhan <abdullayevsarxan92@gmail.com>
2024-05-26 22:45:48 +0400

    adding loading for user photo

--- src/components/psycologyPage.tsx
+++ src/components/psycologyPage.tsx
@@ -52,7 +52,7 @@ const PsyAccount = () => {
   const [showContact, setShowContact] = useState(true);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [messageStatus, setMessageStatus] = useState<string | null>(null);
-  
+  const [isLoading, setIsLoading] = useState(true);
 
 
 
@@ -80,14 +80,15 @@ const PsyAccount = () => {
 
   }, [userSlug]);
 
- useEffect(() => {
+  useEffect(() => {
     if (userId) {
       getUserData(userId).then((userData) => {
         setUserPhoto(userData.photo || '/defaultPhoto.jpg');
+        setIsLoading(false);
       });
     }
   }, [userId]);
-    
+
   const handleSaveChanges = async () => {
     try {
       await updateUser(userSlug, {
@@ -132,11 +133,11 @@ const PsyAccount = () => {
     if (!commentText.trim()) return;
 
     const newComment: Comments = {
-  id: Math.random().toString(36).substring(7),
-  content: commentText,
-  name: userName,
-  photo: userPhoto || '/default_avatar.jpg', 
-  userId,
+      id: Math.random().toString(36).substring(7),
+      content: commentText,
+      name: userName,
+      photo: userPhoto || '/default_avatar.jpg',
+      userId,
     };
     const newCommentsArray = [...comments, newComment]
 
@@ -193,14 +194,14 @@ const PsyAccount = () => {
 
   };
 
- const handleUpdateRating = async (newRating: number) => {
-  try {
-    const ratingId = new Date().toISOString(); 
-    await updateUser(userSlug, { ratings: { [ratingId]: newRating } });
-  } catch (error) {
-    console.error('Error updating rating:', error);
-  }
-};
+  const handleUpdateRating = async (newRating: number) => {
+    try {
+      const ratingId = new Date().toISOString();
+      await updateUser(userSlug, { ratings: { [ratingId]: newRating } });
+    } catch (error) {
+      console.error('Error updating rating:', error);
+    }
+  };
 
 
   const handleUploadImage = async () => {
@@ -232,7 +233,7 @@ const PsyAccount = () => {
       setMessageStatus(`Психолог не может получать уведомления. Пожалуйста, пишите на почтовый адрес: ${email}`);
     }
   };
-  
+
 
 
   return (
@@ -292,13 +293,23 @@ const PsyAccount = () => {
                 <div className="flex items-start ml-5 photo-block">
                   <div className="relative mb-4">
                     <div className="mt-2 mr-5 w-[180px] h-[180px] ">
-                      <Image
-                         src={isEditing ? (imageUrl || userData?.photo || '/default_avatar.jpg') : (userData?.photo || '/default_avatar.jpg')}
-                        alt="User Avatar"
-                        width={180}
-                        height={180}
-                        className="object-cover w-full h-full"
-                      />
+                      {isLoading ? (
+                        <div className="flex justify-center items-center w-full h-full">
+                          <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                          </svg>
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      ) : (
+                        <Image
+                          src={isEditing ? (imageUrl || userData?.photo || '/default_avatar.jpg') : (userData?.photo || '/default_avatar.jpg')}
+                          alt="User Avatar"
+                          width={180}
+                          height={180}
+                          className="object-cover w-full h-full"
+                        />
+                      )}
                     </div>
 
                     {isEditing && (
@@ -343,9 +354,9 @@ const PsyAccount = () => {
                         disabled={!isEditing}
                         maxLength={30}
                       />
-                        <div className='my-1 ml-2 xs:w-[90%]'>
-                        <RatingStars userSlug={userSlug} currentRating={rating} setRating={setRating} userId={userId } />
-                        </div>
+                      <div className='my-1 ml-2 xs:w-[90%]'>
+                        <RatingStars userSlug={userSlug} currentRating={rating} setRating={setRating} userId={userId} />
+                      </div>
                       <button
                         onClick={() => handleSendMessage(userData?.name, userData?.email)}
                         className="bg-blue-500 text-white text-sm px-4 py-2 ml-2 my-2 rounded-md text-center hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 max-w-xs w-auto "
