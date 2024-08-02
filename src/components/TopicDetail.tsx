cf60d139cbd593bb21dda75b3178da5ce47a9da3
Кристина <Kristina@MacBook-Pro.local>
2024-08-02 18:11:30 +0300

    click topic

--- src/components/TopicDetail.tsx
+++ src/components/TopicDetail.tsx
@@ -11,6 +11,10 @@ import { getUsersWithMatchingQuestions } from '@/lib/firebase/firebaseFunctions'
 import Image from 'next/image';
 import icon from '../../public/iconPsy.png';
 import { Data } from '@/interfaces/collections';
+import { openModal } from '@/redux/slices/modalSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/redux/store';
+import { useAppSelector } from '@/redux/hooks';
 
 type Props = {
   videos: Video[]
@@ -23,8 +27,13 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
   const [matchingUsers, setMatchingUsers] = useState<Users[]>([]);
   const params = useParams<{ slug: string }>();
   const router = useRouter();
-
+  const dispatch: AppDispatch = useDispatch();
   const topicSlug = params?.slug ? encodeURIComponent(params.slug) : null;
+  const userId = useAppSelector((state) => state.auth.user?.id);
+
+  const handleOpenModal = () => {
+    dispatch(openModal());
+  };
 
   useEffect(() => {
     if (topicSlug) {
@@ -50,30 +59,6 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
     }
   }, [topicData]);
 
-  if (!topicData) {
-    return (
-      <div>
-        <svg
-          aria-hidden="true"
-          className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-        <span className="sr-only">Loading...</span>
-      </div>
-    );
-  }
-
   const handleClick = async (item: any, type: string, event?: React.MouseEvent<HTMLDivElement>) => {
     let path: any;
     event?.preventDefault();
@@ -84,7 +69,6 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
           .filter(question => question.title !== undefined)
           .find((question) => item.includes(question.title));
 
-
         path = `/questions/${foundQuestion?.slug}`;
         break;
       }
@@ -93,20 +77,30 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
         break;
       }
       case 'profile': {
-        const usersData = await fetchDataFromCollection('users') as Data[];
-        const foundUser = usersData.find(user => user.userId === item);
-
-        if (foundUser) {
-          path = `/profile/${foundUser.slug}`;
-        } else {
-          console.error('User not found');
+        if (!userId) {
+          handleOpenModal(); 
+          console.error("User ID is undefined.");
+          return;
+        }
+      
+        try {
+          const usersData = await fetchDataFromCollection('users') as Data[];
+          const foundUser = usersData.find(user => user.userId === item);
+      
+          if (foundUser) {
+            path = `/profile/${foundUser.slug}`;
+          } else {
+            console.error('User not found');
+          }
+        } catch (error) {
+          console.error("An error occurred while fetching users data:", error);
         }
         break;
       }
       default:
         console.error('Unknown type:', type);
         return;
-    }
+      }
 
     try {
       if (event?.button === 1) {
@@ -115,15 +109,12 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
       } else {
         await router.push(path);
       }
-
     } catch (error) {
       console.error('Navigation error:', error);
     }
   };
 
-  const handleClickTopic = async (url: string, e: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>
-
-  ) => {
+  const handleClickTopic = async (url: string, e: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>) => {
     e.preventDefault();
     try {
       await router.push(url);
@@ -136,42 +127,36 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
     <div className="container mx-auto px-5 py-4 max-w-[1200px] mt-[-20px] justify-center">
       <div className="grid grid-cols-1 md:grid-cols-12 md:mx-auto gap-4 xs:ml-0 sm:ml-0 md:ml-[-20px] md:px-5 lg:ml-[-30px] lg:px-5 xl:ml-[-40px]">
         <div className="order-last pt-3 xs:col-span-12 xs:order-last sm:col-span-12 sm:order-3 md:col-span-8 md:order-last lg:col-span-3 lg:order-first xl:col-span-3 xl:order-first bg-white rounded-lg shadow-lg" style={{ maxHeight: '785px' }}>
-          <VideoGallery topicVideos={topicData.video.map(video => ({ url: video }))}  videosData={videos} />
+          <VideoGallery topicVideos={topicData?.video?.map(video => ({ url: video }))} videosData={videos} />
         </div>
 
-        <div className="xs:col-span-12 xs:pb-5 sm:col-span-12 sm:order-1 sm:mx-auto sm:pb-5 md:col-span-8 md:order-1 lg:col-span-6 xl:col-span-6  bg-white p-6 rounded-lg shadow-lg overflow-hidden" style={{ maxHeight: '790px' }}>
-          <h2 className="w-full font-semibold bg-amber-300 text-gray-600 text-base px-7 py-3 rounded-2xl leading-6 text-center">{topicData.title}</h2>
+        <div className="xs:col-span-12 xs:pb-5 sm:col-span-12 sm:order-1 sm:mx-auto sm:pb-5 md:col-span-8 md:order-1 lg:col-span-6 xl:col-span-6 bg-white p-6 rounded-lg shadow-lg overflow-hidden" style={{ maxHeight: '790px' }}>
+          <h2 className="w-full font-semibold bg-amber-300 text-gray-600 text-base px-7 py-3 rounded-2xl leading-6 text-center">{topicData?.title}</h2>
           <h1 className="font-bold text-black-600 text-lg text-center mt-5 mb-5">Вопросы</h1>
           <div className="flex flex-col space-y-4" style={{ maxHeight: '290px', overflowY: 'auto', paddingBottom: '10px' }}>
-            {topicData.questions.map((question: any, index: number) => (
-
+            {topicData?.questions?.map((question: any, index: number) => (
               <div
                 key={`question-${index}`}
                 onClick={(e) => handleClick(question, 'question', e)}
-
                 role="button"
                 tabIndex={0}
                 onKeyDown={(e) => e.key === 'Enter' && handleClick(question, 'question')}
-
                 onMouseDown={(e) => {
                   if (e.button === 1) {
                     e.preventDefault();
                     handleClick(question, 'question', e);
                   }
                 }}
-
-
                 className="bg-white mx-1 p-3 rounded-lg shadow-md cursor-pointer hover:bg-gray-100 focus:bg-gray-100"
               >
                 <h2 className="text-base font-semibold">{question}</h2>
               </div>
-
             ))}
           </div>
 
           <h1 className="font-bold text-black-600 text-lg text-center mt-5 mb-6">Статьи</h1>
           <div className="flex flex-col space-y-4" style={{ maxHeight: '290px', overflowY: 'auto', paddingBottom: '10px' }}>
-            {topicData.articles.map((article: any, index: number) => (
+            {topicData?.articles?.map((article: any, index: number) => (
               <Link key={article.slug || article.id} href={`/articles/${article.slug || article.id}`}>
                 <div
                   key={`article-${index}`}
@@ -194,7 +179,7 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
             {matchingUsers.map((user) => (
               <div key={user.userId} className="text-center">
                 <div
-                  onClick={() => handleClick(user.userId, 'profile')}
+                  onClick={(e) => handleClick(user.userId, 'profile', e)}
                   role="button"
                   tabIndex={0}
                   onKeyDown={(e) => e.key === 'Enter' && handleClick(user.userId, 'profile')}
@@ -216,7 +201,7 @@ const TopicDetail: React.FC<Props> = ({ videos }) => {
           </button>
         </Link>
       </div>
-    </div >
+    </div>
   );
 };
 
